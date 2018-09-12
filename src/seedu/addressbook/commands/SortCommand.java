579457b88d2d475724946233a9384dfdbdadc9fe
Winson <winsonheng@hotmail.com>
2018-09-12 20:09:16 +0800

    Added sort command

---
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.commands;
+
+public class SortCommand extends Command {
+
+    public static final String COMMAND_WORD = "find";
+
+    @Override
+    public CommandResult execute() {
+        return new CommandResult("Lawl");
+    }
+}
